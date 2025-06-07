eb5bc6d6a5caa299731013400bdab74f1278e925
Fawaz Adeniji <fawazadeniji123@gmail.com>
2025-06-07 21:11:11 +0100

    Update CORS options to restrict allowed origins to specific URLs

--- src/middleware/corsOptions.js
+++ src/middleware/corsOptions.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 export const corsOptions = cors({
-  origin: ['*', 'http://localhost:3001'],
+  origin: ['http://localhost:3000', 'http://localhost:3001'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 });

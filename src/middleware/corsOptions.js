d82774ae1e413e16d0dc8a937efd68e7c71eb98d
Fawaz Adeniji <fawazadeniji123@gmail.com>
2025-06-08 08:06:21 +0100

    Update CORS options to include production URL for enhanced cross-origin requests

--- src/middleware/corsOptions.js
+++ src/middleware/corsOptions.js
@@ -1,7 +1,11 @@
 import cors from 'cors';
 
 export const corsOptions = cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://attendance-system-red.vercel.app/',
+  ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 });
